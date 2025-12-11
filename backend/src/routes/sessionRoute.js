ef91482a1352672358e55673d1bfee791c85a72d
Mayankbhardwaj255 <mayankbhardwaj708@gmail.com>
2025-12-11 23:47:04 +0530

    Add session Route

---
+++ backend/src/routes/sessionRoute.js
@@ -0,0 +1,18 @@
+import express from "express";
+import { protectRoute } from "../middleware/protectRoute.js";
+import {
+  createSession,
+  endSession,
+  getActiveSessions,
+  getMyRecentSessions,
+  getSessionById,
+  joinSession,
+} from "../controllers/sessionController.js";
+
+const router = express.Router();
+
+router.post("/", protectRoute, createSession);
+router.get("/active", protectRoute, getActiveSessions);
+router.get("/my-recent", protectRoute, getMyRecentSessions);
+
+export default router;
