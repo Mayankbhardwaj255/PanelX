ef3cd0749618469ba6e01b21fb5335c891e2d7ea
Mayankbhardwaj255 <mayankbhardwaj708@gmail.com>
2025-12-12 23:52:19 +0530

    Update session route

--- backend/src/routes/sessionRoute.js
+++ backend/src/routes/sessionRoute.js
@@ -15,4 +15,10 @@ router.post("/", protectRoute, createSession);
 router.get("/active", protectRoute, getActiveSessions);
 router.get("/my-recent", protectRoute, getMyRecentSessions);
 
+
+router.get("/:id", protectRoute, getSessionById);
+router.post("/:id/join", protectRoute, joinSession);
+router.post("/:id/end", protectRoute, endSession);
+
+
 export default router;
